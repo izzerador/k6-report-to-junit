adfa7e7a681d392072a9b63932f75585d3e96517
Sergey Ryzhkov <ryzhkov@kolesa.kz>
2021-01-28 12:04:15 +0600

    ещё в одном месте

--- index.js
+++ index.js
@@ -15,17 +15,15 @@ const suite = builder.testSuite().name('test');
 
 for (let i = 0; i < data.length; i++) { 
     for (info in data[i]) {
-
+        let className = data[i][info].path ? data[i][info].path : 'Test Name Not Found';
+        let testName = data[i][info].name ? data[i][info].name : 'Check name not found';
         if(data[i][info].fails && data[i][info].name) {
-            let className = data[i][info].path ? data[i][info].path : 'Test Name Not Found';
-            let testName = data[i][info].name ? data[i][info].name : 'Check name not found';
-
             suite.testCase().className(className).name(testName).failure();
 
             continue;
         }
 
-        suite.testCase().className(data[i][info].path).name(data[i][info].name)       
+        suite.testCase().className(className).name(testName)       
     }
   }
 
