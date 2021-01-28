2963ecdc00b278244da3a4c01b070eedb95f479b
Sergey Ryzhkov <ryzhkov@kolesa.kz>
2021-01-28 12:00:57 +0600

    исправляю комментарий, фикшу падение

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ data = data.filter(el => {
     return Object.keys(el).length > 0; 
 });
 
-data.splice(0, 1) // in first object has no test case info
+data.splice(0, 1) // first object is empty
 
 const suite = builder.testSuite().name('test');
 
@@ -17,7 +17,10 @@ for (let i = 0; i < data.length; i++) {
     for (info in data[i]) {
 
         if(data[i][info].fails && data[i][info].name) {
-            suite.testCase().className(data[i][info].path).name(data[i][info].name).failure();
+            let className = data[i][info].path ? data[i][info].path : 'Test Name Not Found';
+            let testName = data[i][info].name ? data[i][info].name : 'Check name not found';
+
+            suite.testCase().className(className).name(testName).failure();
 
             continue;
         }
